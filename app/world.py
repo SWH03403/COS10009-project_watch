9472bf631cf3e9c98032874e401dbbd6c7fe5659
SWH03403 <106224482@student.swin.edu.au>
2026-03-10 08:51:48 +0700

    feat: load door & room connection

--- app/world.py
+++ app/world.py
@@ -26,9 +26,19 @@ class Room:
 	fog: Fog = field(default_factory=Fog)
 
 @dataclass
+class RoomConnection:
+	idx: int
+	wall: int
+	offset: float
+
+@dataclass
 class Door:
-	closable: bool = False
-	locked: bool = False
+	width: float
+	room_from: RoomConnection
+	room_to: RoomConnection
+	is_opened: bool = True
+	is_locked: bool = False
+	is_visible: bool = True
 
 @dataclass
 class Edge:
@@ -40,7 +50,7 @@ class Level:
 	spawn: Vec2 = field(default_factory=Vec2)
 	rooms: list[Room] = field(default_factory=list)
 	doors: list[Door] = field(default_factory=list)
-	_edges: dict[int, list[Edge]] = field(default_factory=dict)
+	edges: dict[int, list[Edge]] = field(default_factory=dict)
 
 @dataclass
 class LevelLoader:
@@ -50,6 +60,10 @@ class LevelLoader:
 
 	def __del__(self) -> None:
 		self._file.close()
+
+	def _add_edge(self, r1: int, r2: int, door: int) -> None:
+		if r1 not in self.level.edges: self.level.edges[r1] = []
+		self.level.edges[r1].append(Edge(r2, door))
 
 	def _parse_spawn(self, args: list[str]) -> None:
 		self.level.spawn = Vec2(float(args[0]), float(args[1]))
@@ -66,15 +80,38 @@ class LevelLoader:
 		room = Room(corners)
 		self.level.rooms.append(room)
 
+	def _parse_connection(self, word: str, first: bool) -> RoomConnection:
+		args = word.split(",")
+		room = len(self.level.rooms) - 1 if args[0] == "" else int(args[0])
+		if first and args[0] == "": room -= 1
+		if room < 0: room += len(self.level.rooms)
+		wall, offset = int(args[1]), float(args[2])
+		return RoomConnection(room, wall, offset)
+
+	def _parse_door(self, args: list[str]) -> None:
+		width = float(args[0])
+		room_from = self._parse_connection(args[1], True)
+		room_to = self._parse_connection(args[2], False)
+		door = Door(width, room_from, room_to)
+
+		idx = len(self.level.doors)
+		self.level.doors.append(door)
+		self._add_edge(room_from.idx, room_to.idx, idx)
+		self._add_edge(room_to.idx, room_from.idx, idx)
+
 	def __post_init__(self) -> None:
 		self._file = open(f"assets/levels/{self.name}.txt", "r", encoding="utf-8")
 		for line in self._file.readlines():
-			args = line.strip().split(" ")
+			line = line.split("#")[0].strip()
+			if len(line) == 0: continue
+			args = line.split(" ")
 			match args.pop(0):
 				case "s":
 					self._parse_spawn(args)
 				case "r4":
 					self._parse_quad_room(args)
+				case "d":
+					self._parse_door(args)
 
 	def into_level(self) -> Level:
 		return self.level
