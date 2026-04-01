04ba7a3191d1acf6dd9985902aacebe2280012ad
SWH03403 <106224482@student.swin.edu.au>
2026-04-01 22:37:59 +0700

    feat: render neighbor sectors

--- game/render/__init__.py
+++ game/render/__init__.py
@@ -25,7 +25,7 @@ def init() -> None:
 	global I
 	I = Renderer(queue=[], rendered_sectors=set())
 
-def _render_sector(window: Window) -> None:
+def _render_sector(window: Window) -> list[Window]:
 	level = game.get_level()
 	sector = level.sectors[window.sector]
 	vertexes = [level.vertexes[idx] for idx in sector.vertexes]
@@ -34,6 +34,7 @@ def _render_sector(window: Window) -> None:
 
 	n_walls = len(sector.vertexes)
 	walls = [(vertexes[i], vertexes[i - n_walls + 1], sector.connects[i]) for i in range(n_walls)]
+	queued_neighbors = []
 	for left, right, connect in walls:
 		# only render if facing the player
 		facing = math.atan2(left.x, left.y) - math.atan2(right.x, right.y)
@@ -58,14 +59,17 @@ def _render_sector(window: Window) -> None:
 		l_top, l_bot = world_to_screen(left, sector.floor, sector.ceiling)
 		r_top, r_bot = world_to_screen(right, sector.floor, sector.ceiling)
 
-		nl_top, nl_bot, nr_top, nr_bot = None, None, None, None
-		if connect is not None:
-			neighbor = level.sectors[connect]
-			nl_top, nl_bot = world_to_screen(left, neighbor.floor, neighbor.ceiling)
-			nr_top, nr_bot = world_to_screen(right, neighbor.floor, neighbor.ceiling)
-
 		left_x = int(clamp(l_top.x, window.top_left.x, window.top_right.x))
 		right_x = int(clamp(r_top.x, window.top_left.x, window.top_right.x))
+
+		# get screen coordinate for window to neighbor sector
+		nl_top, nl_bot, nr_top, nr_bot = None, None, None, None
+		next_window = [] # a bad attempt to find render region
+		if connect is not None:
+			queued = level.sectors[connect]
+			nl_top, nl_bot = world_to_screen(left, queued.floor, queued.ceiling)
+			nr_top, nr_bot = world_to_screen(right, queued.floor, queued.ceiling)
+
 		for x in range(left_x, right_x):
 			fact = invlerp(l_top.x, r_top.x, x)
 			proj_x = lerp(left.x, right_proj_x, fact)
@@ -92,6 +96,19 @@ def _render_sector(window: Window) -> None:
 				if ntop > top: draw.line(screen, blended, (x, top), (x, ntop))
 				if nbot < bot: draw.line(screen, blended, (x, nbot), (x, bot))
 
+				if x == left_x or x == right_x - 1:
+					y = max(lerp(nl_top.y, nr_top.y, fact), lerp(l_top.y, r_top.y, fact))
+					next_window.append(Vec2(x, y))
+					y = min(lerp(nl_bot.y, nr_bot.y, fact), lerp(l_bot.y, r_bot.y, fact))
+					next_window.append(Vec2(x, y))
+
+		if connect is not None and len(next_window) == 4:
+			tl, bl, tr, br = next_window
+			queued = Window(sector=connect, top_left=tl, top_right=tr, bottom_left=bl, bottom_right=br)
+			queued_neighbors.append(queued)
+
+	return queued_neighbors
+
 def update() -> None:
 	I.queue = [current_sector()]
 	I.rendered_sectors.clear()
@@ -100,4 +117,4 @@ def update() -> None:
 		window = I.queue.pop(0)
 		if window.sector in I.rendered_sectors: continue
 		I.rendered_sectors.add(window.sector)
-		_render_sector(window)
+		I.queue.extend(_render_sector(window))
