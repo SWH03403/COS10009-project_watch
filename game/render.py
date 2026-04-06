edd62e725fd6eafaa087858da90499d7e01ca30a
SWH03403 <106224482@student.swin.edu.au>
2026-04-06 22:03:59 +0700

    refactor: clearer var names & cleaner routines

--- game/render.py
+++ game/render.py
@@ -38,14 +38,13 @@ def debug_delay() -> None:
 		sleep(5e-3)
 		pygame.display.update()
 
-def world_to_screen(p: Vec2, floor: float, ceiling: float) -> tuple[Vec2, Vec2]:
-	w, h = engine.get_screen().get_size()
-	eye = player.get_absolute_eye_height()
+def world_y_to_screen(y: float, z: float) -> float:
+	h = engine.get_screen().get_height()
+	return remap(0, 1, h / 2, 0, z / y)
 
-	x = remap(-1, 1, 0, w, p.x / p.y)
-	y1 = remap(0, 1, h / 2, 0, (ceiling - eye) / p.y)
-	y2 = remap(0, 1, h / 2, h, (eye - floor) / p.y)
-	return Vec2(x, y1), Vec2(x, y2)
+def world_to_screen(p: Vec2, z: float) -> Vec2:
+	w = engine.get_screen().get_width()
+	return Vec2(remap(-1, 1, 0, w, p.x / p.y), world_y_to_screen(p.y, z))
 
 def line(color: Color, x: float, y1: float, y2: float) -> None:
 	screen = engine.get_screen()
@@ -57,30 +56,32 @@ def render_floor(
 	color: ColorLike,
 	fog: Fog,
 	mask: list[tuple[int, int]],
-	wall: Line,
-	right_further: bool,
+	left: Vec2,
+	right: Vec2,
 	x_range: tuple[int, int],
-	y_range: tuple[int, int],
-	y_fog_levels: tuple[float, float], # screen coordinate of near & far
+	y_range: range,
 ) -> None:
 	screen = engine.get_screen()
-	left, right = x_range
 	near_on_far = Line.from_point(Vec2(fog.far, eye_diff)).get_y(fog.near)
-	for y in range(*y_range):
-		for min_x in range(left, len(I.mask)):
+	y_fog_near = world_y_to_screen(fog.near, eye_diff)
+	y_fog_far = world_y_to_screen(fog.far, eye_diff)
+	right_further = left.y * eye_diff < right.y * eye_diff # multiply for sign only
+
+	for y in y_range:
+		for min_x in range(x_range.start, len(I.mask)):
 			min_y, max_y = mask[min_x]
 			if min_y <= y <= max_y: break
-		for max_x in range(right - 1, -1, -1):
+		for max_x in range(x_range.stop - 1, -1, -1):
 			min_y, max_y = mask[max_x]
 			if min_y <= y <= max_y: break
 
-		if not wall.is_horz():
-			intersect = wall.get_x(y)
+		if left.y != right.y:
+			intersect = Line.from_point(left, right).get_x(y)
 			if right_further: min_x = max(min_x, intersect)
 			else: max_x = min(max_x, intersect)
 		if min_x >= max_x: continue
 
-		fact = invlerp(*y_fog_levels, y)
+		fact = invlerp(y_fog_near, y_fog_far, y)
 		blended = Color(color)
 		if fact >= 1: blended = fog.color
 		elif fact > 0:
@@ -98,17 +99,13 @@ def render_sector(scoped: ScopedSector) -> None:
 	screen = engine.get_screen()
 	h = screen.get_height()
 
-	eye = player.get_absolute_eye_height()
-	eye_to_floor = eye - sector.floor
-	eye_to_ceil = sector.ceiling - eye
+	z_player = player.get_absolute_eye_height()
+	z_floor = sector.floor - z_player
+	z_ceil = sector.ceiling - z_player
 	fog = level.fog # FIX: use sector-specific fog
-	fog_coords = []
-	fog_coords.extend(world_to_screen(Vec2(0, fog.near), sector.floor, sector.ceiling))
-	fog_coords.extend(world_to_screen(Vec2(0, fog.far), sector.floor, sector.ceiling))
-	fog_ys = [int(p.y) for p in fog_coords]
 
 	walls = get_walls(level, scoped.id, True)
-	for left, right, connect in walls:
+	for left, right, neighbor_id in walls:
 		# only render if facing the player
 		facing = math.atan2(left.x, left.y) - math.atan2(right.x, right.y)
 		if 0 <= facing <= math.pi: continue
@@ -127,76 +124,77 @@ def render_sector(scoped: ScopedSector) -> None:
 		right_proj = Line.from_point(right)
 		right_proj_x = right_proj.get_x(left.y)
 
-		l_top, l_bot = world_to_screen(left, sector.floor, sector.ceiling)
-		r_top, r_bot = world_to_screen(right, sector.floor, sector.ceiling)
+		# wall corners
+		left_top = world_to_screen(left, z_ceil)
+		left_bottom = world_to_screen(left, z_floor)
+		right_top = world_to_screen(right, z_ceil)
+		right_bottom = world_to_screen(right, z_floor)
 
-		left_x = int(clamp(l_top.x, scoped.min_x, scoped.max_x))
-		right_x = int(clamp(r_top.x, scoped.min_x, scoped.max_x))
+		# range of visible x
+		left_x = int(clamp(left_top.x, scoped.min_x, scoped.max_x))
+		right_x = int(clamp(right_top.x, scoped.min_x, scoped.max_x))
 
 		# extend to edge of scope if wall plane cuts through eye
-		if abs(l_top.y) >= EXTEND_THRESHOLD: left_x = scoped.min_x
-		if abs(r_top.y) >= EXTEND_THRESHOLD: right_x = scoped.max_x
+		if abs(left_top.y) >= EXTEND_THRESHOLD: left_x = scoped.min_x
+		if abs(right_top.y) >= EXTEND_THRESHOLD: right_x = scoped.max_x
 
 		# get screen coordinate for window to neighbor sector
-		nl_top, nl_bot, nr_top, nr_bot = None, None, None, None
-		if connect is not None:
-			neighbor = level.sectors[connect]
-			nl_top, nl_bot = world_to_screen(left, neighbor.floor, neighbor.ceiling)
-			nr_top, nr_bot = world_to_screen(right, neighbor.floor, neighbor.ceiling)
-			I.queue.append(ScopedSector(connect, left_x, right_x))
+		nb_left_top, nb_left_bottom, nb_right_top, nb_right_bottom = None, None, None, None
+		if neighbor_id is not None:
+			neighbor = level.sectors[neighbor_id]
+			z_nb_floor = neighbor.floor - z_player
+			z_nb_ceil = neighbor.ceiling - z_player
+			nb_left_top = world_to_screen(left, z_nb_ceil)
+			nb_left_bottom = world_to_screen(left, z_nb_floor)
+			nb_right_top = world_to_screen(right, z_nb_ceil)
+			nb_right_bottom = world_to_screen(right, z_nb_floor)
+			I.queue.append(ScopedSector(neighbor_id, left_x, right_x))
 
 		last_mask = copy(I.mask)
 		flrs = [h, 0, h, 0]
 
-		for x in range(left_x, right_x):
+		xs = range(left_x, right_x)
+		for x in xs:
 			min_y, max_y = I.mask[x]
 			if min_y >= max_y: continue
 
 			camera_dist = 0
-			if l_top.x != r_top.x:
-				fact = invlerp(l_top.x, r_top.x, x)
-				proj_x = lerp(left.x, right_proj_x, fact)
-				proj_vec = Vec2(proj_x, left.y)
-				if abs(proj_vec.cross(left - right)) > proj_vec.epsilon:
-					proj = Line.from_point(proj_vec)
-					world_pos = proj.intersect(wall)
+			if left_top.x != right_top.x:
+				fact = invlerp(left_top.x, right_top.x, x)
+				projection = Vec2(lerp(left.x, right_proj_x, fact), left.y)
+				if abs(projection.cross(left - right)) > projection.epsilon:
+					world_pos = Line.from_point(projection).intersect(wall)
 					camera_dist = world_pos.length()
-			else:
-				fact = 1 if x > l_top.x else 0
+			else: # wall plane cut through eye, becoming infinitely thin
+				fact = 1 if x > left_top.x else 0
 
 			fog_amt = clamp(invlerp(fog.near, fog.far, camera_dist), 0, 1) * fog.intensity
 			blended = Color("darkkhaki").lerp(fog.color, fog_amt)
-			top = int(clamp(lerp(l_top.y, r_top.y, fact), min_y, max_y))
-			bot = int(clamp(lerp(l_bot.y, r_bot.y, fact), min_y, max_y))
+			top = int(clamp(lerp(left_top.y, right_top.y, fact), min_y, max_y))
+			bottom = int(clamp(lerp(left_bottom.y, right_bottom.y, fact), min_y, max_y))
 
-			if connect is None:
-				line(blended, x, top, bot) # solid wall
+			if neighbor_id is None:
+				line(blended, x, top, bottom) # solid wall
 				I.mask[x] = (-1, -1)
 			else:
-				ntop = int(clamp(lerp(nl_top.y, nr_top.y, fact), min_y, max_y))
-				nbot = int(clamp(lerp(nl_bot.y, nr_bot.y, fact), min_y, max_y))
-				I.mask[x] = (max(top, ntop), min(bot, nbot))
+				nb_top = int(clamp(lerp(nb_left_top.y, nb_right_top.y, fact), min_y, max_y))
+				nb_bottom = int(clamp(lerp(nb_left_bottom.y, nb_right_bottom.y, fact), min_y, max_y))
+				I.mask[x] = (max(top, nb_top), min(bottom, nb_bottom))
 				if game.is_scan_mode(): line("crimson", x, *I.mask[x])
-				if ntop > top: line(blended, x, top, ntop)
-				if nbot < bot: line(blended, x, nbot, bot)
+				if nb_top > top: line(blended, x, top, nb_top)
+				if nb_bottom < bottom: line(blended, x, nb_bottom, bottom)
 
 			flrs[0] = min(flrs[0], min_y)
 			flrs[1] = max(flrs[1], top)
-			flrs[2] = min(flrs[2], bot)
+			flrs[2] = min(flrs[2], bottom)
 			flrs[3] = max(flrs[3], max_y)
 
-		if eye_to_ceil > 0:
-			right_further = l_top.y < r_top.y
-			render_floor(
-				eye_to_ceil, "khaki4", fog, last_mask, Line.from_point(l_top, r_top), right_further,
-				(left_x, right_x), (flrs[0], flrs[1]), (fog_ys[0], fog_ys[2])
-			)
-		if eye_to_floor > 0:
-			right_further = l_bot.y > r_bot.y
-			render_floor(
-				eye_to_floor, "darkslategrey", fog, last_mask, Line.from_point(l_bot, r_bot), right_further,
-				(left_x, right_x), (flrs[2], flrs[3] + 1), (fog_ys[1], fog_ys[3])
-			)
+		if z_ceil > 0:
+			ys = range(flrs[0], flrs[1])
+			render_floor(z_ceil, "khaki4", fog, last_mask, left_top, right_top, xs, ys)
+		if z_floor < 0:
+			ys = range(flrs[2], flrs[3] + 1)
+			render_floor(z_floor, "darkslategrey", fog, last_mask, left_bottom, right_bottom, xs, ys)
 
 def update() -> None:
 	w, h = engine.get_screen().get_size()
