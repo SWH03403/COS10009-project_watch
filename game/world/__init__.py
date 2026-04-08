c79c598834184fa7e83ff1d04b3fa394d20c380d
SWH03403 <106224482@student.swin.edu.au>
2026-04-08 09:18:56 +0700

    refactor: avoid circular imports

--- game/world/__init__.py
+++ game/world/__init__.py
@@ -1,4 +1,14 @@
+from pygame import Vector2
+
 from .fog import Fog, default_fog
 from .level import Level
 from .sector import Sector, Plane, Wall
 from .spawn import Spawn
+
+def get_walls(level: Level, sector_id: int, relative: bool) -> list[tuple[Vector2, Vector2, Wall]]:
+	from game.entity import player
+	sector = level.sectors[sector_id]
+	n_walls = len(sector.walls)
+	v = [level.vertexes[wall.vertex] for wall in sector.walls]
+	v = [player.get_relative(p) for p in v] if relative else v
+	return [(v[i], v[i - n_walls + 1], sector.walls[i]) for i in range(n_walls)]
--- game/world/level.py
+++ game/world/level.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
 from pygame import Vector2
 
-from game.entity import player
-from .sector import Sector, Wall
+from .sector import Sector
 from .spawn import Spawn
 
 @dataclass
@@ -10,10 +9,3 @@ class Level:
 	spawns: list[Spawn]
 	vertexes: list[Vector2]
 	sectors: list[Sector]
-
-def get_walls(level: Level, sector_id: int, relative: bool) -> list[tuple[Vector2, Vector2, Wall]]:
-	sector = level.sectors[sector_id]
-	n_walls = len(sector.walls)
-	v = [level.vertexes[wall.vertex] for wall in sector.walls]
-	v = [player.get_relative(p) for p in v] if relative else v
-	return [(v[i], v[i - n_walls + 1], sector.walls[i]) for i in range(n_walls)]
