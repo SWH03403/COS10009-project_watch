7a73f4f00b52c06a77d111801327dabd1879252e
SWH03403 <106224482@student.swin.edu.au>
2026-04-08 07:19:04 +0700

    feat(entity): add creature emotion state

--- game/entity/creature.py
+++ game/entity/creature.py
@@ -5,6 +5,7 @@ from pygame import Vector2
 class Creature:
 	position: Vector2
 	maintain_distance: tuple[float, float]
+	aggressive: bool
 
 I: Creature
 
@@ -13,7 +14,11 @@ def init() -> None:
 	I = Creature(
 		position=Vector2(100, 100),
 		maintain_distance=(100, 200),
+		aggressive=False,
 	)
 
 def get_position() -> Vector2:
 	return I.position
+
+def is_aggressive() -> bool:
+	return I.aggressive
