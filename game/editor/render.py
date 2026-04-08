f1b99090f8cc50501c7fdfe932d9404aba27ff0f
SWH03403 <106224482@student.swin.edu.au>
2026-04-08 13:41:25 +0700

    feat: draw walls once

--- game/editor/render.py
+++ game/editor/render.py
@@ -1,20 +1,24 @@
 import math
 import pygame
+from pygame import Vector2
 
+import game
 from game import engine
 from .. import editor
 
 GRID_COLOR: str = "gray12"
-GRID_COLOR_ORIGIN: str = "gray60"
+GRID_COLOR_ORIGIN: str = "gray36"
 LINE_SPACING: float = 50
-LINE_WIDTH: float = 1
+
+def get_line_width(adhoc_scale: float = 1) -> int:
+	return max(round(editor.get_zoom() * adhoc_scale), 1)
 
 def render_grid() -> None:
 	screen = engine.get_screen()
 	w, h = screen.size
 	scale = editor.get_zoom()
 	spacing = LINE_SPACING * scale
-	lw = max(int(LINE_WIDTH * scale), 1)
+	lw = get_line_width()
 	origin = editor.get_origin()
 
 	nx, ny = math.ceil(w / spacing), math.ceil(h / spacing)
@@ -42,5 +46,27 @@ def render_grid() -> None:
 				y = y0 + spacing * (j + (pj + 1) / 5)
 				pygame.draw.circle(screen, GRID_COLOR, (x, y), lw)
 
+def xy_to_screen(p: Vector2) -> Vector2:
+	scaled = p * editor.get_zoom()
+	scaled.y *= -1
+	return editor.get_origin() + scaled
+
+def render_level() -> None:
+	screen = engine.get_screen()
+	level = game.get_level()
+	lw = get_line_width(1.5)
+	rendered_wall = set()
+	for sector in level.sectors:
+		for i, wall in enumerate(sector.walls):
+			left, right = wall.vertex, sector.walls[i - len(sector.walls) + 1].vertex
+			if left > right: left, right = right, left
+			if (left, right) in rendered_wall: continue
+			rendered_wall.add((left, right)) # FIX: consider different wall types
+
+			left = xy_to_screen(level.vertexes[left])
+			right = xy_to_screen(level.vertexes[right])
+			pygame.draw.line(screen, "white", left, right, lw)
+
 def perform() -> None:
 	render_grid()
+	render_level()
