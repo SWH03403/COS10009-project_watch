e48f374c89396563317e90a96105055de843c8c3
SWH03403 <106224482@student.swin.edu.au>
2026-04-08 10:23:44 +0700

    feat: editor grid

--- game/editor/__init__.py
+++ game/editor/__init__.py
@@ -12,17 +12,23 @@ from . import render
 class MapEditor:
 	level: Level
 	zoom: float
-	position: Vector2
+	origin: Vector2 # position of world coordinate (0, 0) on the screen
 
 I: MapEditor = None
 
 def init() -> None:
 	global I
-	position = -Vector2(engine.get_screen().size) / 2
-	I = MapEditor(level=game.get_level(), zoom=1, position=position)
+	origin = Vector2(engine.get_screen().size) / 2
+	I = MapEditor(level=game.get_level(), zoom=0, origin=origin)
 
 def get_init() -> bool:
 	return I is not None
+
+def get_zoom() -> float:
+	return 2**I.zoom
+
+def get_origin() -> Vector2:
+	return I.origin * get_zoom()
 
 def handle_keydown(key: int) -> None:
 	match key:
--- game/editor/render.py
+++ game/editor/render.py
@@ -1,2 +1,45 @@
+import pygame
+
+from game import engine
+from .. import editor
+
+GRID_COLOR: str = "gray12"
+GRID_COLOR_ORIGIN: str = "gray60"
+LINE_SPACING: float = 50
+LINE_WIDTH: float = 1
+
+def render_grid() -> None:
+	screen = engine.get_screen()
+	w, h = screen.size
+	scale = editor.get_zoom()
+	spacing = LINE_SPACING * scale
+	lw = max(int(LINE_WIDTH * scale), 1)
+	origin = editor.get_origin()
+
+	nx, ny = int(w / spacing), int(h / spacing)
+	x0, y0 = int(origin.x % spacing), int(origin.y % spacing)
+	for i in range(nx):
+		x = x0 + spacing * i
+		if x == origin.x: continue
+		pygame.draw.line(screen, GRID_COLOR, (x, 0), (x, h), lw)
+	for i in range(ny):
+		y = y0 + spacing * i
+		if y == origin.y: continue
+		pygame.draw.line(screen, GRID_COLOR, (0, y), (w, y), lw)
+
+	# render origin line last so it's on top
+	if 0 <= origin.x <= w:
+		pygame.draw.line(screen, GRID_COLOR_ORIGIN, (origin.x, 0), (origin.x, h), lw)
+	if 0 <= origin.y <= h:
+		pygame.draw.line(screen, GRID_COLOR_ORIGIN, (0, origin.y), (w, origin.y), lw)
+
+	# render dots
+	for i in range(-1, nx):
+		for j in range(-1, ny):
+			for pi, pj in [(0, 0), (0, 3), (3, 0), (3, 3)]:
+				x = x0 + spacing * (i + (pi + 1) / 5)
+				y = y0 + spacing * (j + (pj + 1) / 5)
+				pygame.draw.circle(screen, GRID_COLOR, (x, y), lw)
+
 def perform() -> None:
-	...
+	render_grid()
