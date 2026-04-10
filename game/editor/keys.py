cfc9d2913e7dc9c0fc89044b1b151c8251f9a6d5
SWH03403 <106224482@student.swin.edu.au>
2026-04-10 12:16:56 +0700

    feat: function to switch wall side

--- game/editor/keys.py
+++ game/editor/keys.py
@@ -5,6 +5,7 @@ from game.world import Wall
 from game.world.sector import WallType, set_wall_type
 from .. import editor
 from . import cache, selection
+from .cache import SectorRef
 from .common import EditMode, is_shift_held
 
 def insert_vertex() -> None:
@@ -38,28 +39,32 @@ def insert_vertex() -> None:
 
 	editor.set_selection(selection.Vertex(id=vertex_id))
 
-def set_selection_wall_type(typ: WallType, onesided: bool) -> None:
+def get_neighbor_ref() -> SectorRef | None:
 	sel = editor.get_selection()
 	if not isinstance(sel, selection.Wall): return
 	level = game.get_level()
-
 	sector = level.sectors[sel.sector_id]
-	wall = sector.walls[sel.wall_idx]
-	left = wall.vertex
+	left = sector.walls[sel.wall_idx].vertex
 	right = sector.walls[sel.wall_idx - len(sector.walls) + 1].vertex
 
-	nb_id = nb_wall = None
 	for ref in cache.get_sectors(left, right):
 		# FIX: verify walls are opposite facing
-		if ref.id != sel.sector_id:
-			nb_id = ref.id
-			nb_wall = level.sectors[ref.id].walls[ref.wall_idx]
-			break
+		if ref.id != sel.sector_id: return ref
+
+def set_selection_wall_type(typ: WallType, onesided: bool) -> None:
+	ref = get_neighbor_ref()
+	if ref is None: return
+	sel = editor.get_selection()
+	assert isinstance(sel, selection.Wall)
+
+	level = game.get_level()
+	wall = level.sectors[sel.sector_id].walls[sel.wall_idx]
+	nb_wall = level.sectors[ref.id].walls[ref.wall_idx]
 
 	onesided |= nb_wall is None
 	if typ == WallType.NEIGHBOR:
 		set_wall_type(wall, WallType.SOLID)
-		wall.neighbor = nb_id
+		wall.neighbor = ref.id
 		if not onesided:
 			set_wall_type(nb_wall, WallType.SOLID)
 			nb_wall.neighbor = sel.sector_id
@@ -68,6 +73,11 @@ def set_selection_wall_type(typ: WallType, onesided: bool) -> None:
 		if not onesided: set_wall_type(nb_wall, typ)
 
 	cache.set_expired_walls() # NOTE: this must only be called after wall types are changed
+
+def switch_wall_side() -> None:
+	ref = get_neighbor_ref()
+	if ref is None: return
+	editor.set_selection(selection.Wall(sector_id=ref.id, wall_idx=ref.wall_idx))
 
 def handle_keydown(key: int) -> None:
 	shift = is_shift_held()
@@ -90,6 +100,8 @@ def handle_keydown(key: int) -> None:
 			sel = editor.get_selection()
 			if isinstance(sel, (selection.Vertex, selection.Wall)):
 				editor.set_mode(EditMode.ADD)
+		case pygame.K_s:
+			switch_wall_side()
 		case pygame.K_1:
 			set_selection_wall_type(WallType.SOLID, shift)
 		case pygame.K_2:
