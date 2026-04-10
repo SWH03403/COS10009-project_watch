2bd866052e24dd7f0975787a2164c603ee6b9c5d
SWH03403 <106224482@student.swin.edu.au>
2026-04-10 22:44:38 +0700

    feat(assets): library with key for variants

---
+++ game/assets/library.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from enum import Enum
+import random
+from pygame import Sound as SoundFile, Surface
+from . import loaders
+
+class Image(Enum):
+	CREATURE_FLOAT = "creature/float"
+	CREATURE_GRAB = "creature/grab"
+	SKYBOX_CLOUDY = "skybox/cloudy"
+	WINDOW_ICON = "icon"
+	VIGNETTE = "vignette"
+
+class Sound(Enum):
+	AMBIENT_WINDY = "windy"
+	DEATH_CAUGHT = "death/caught"
+	DEATH_FALL = "death/fall"
+	STEP_CONCRETE = "footsteps/concrete"
+	STEP_METAL = "footsteps/metal"
+	STEP_TILE = "footsteps/tile"
+	STEP_WOOD = "footsteps/wood"
+	STEP_WOOD_PANEL = "footsteps/woodpanel"
+
+@dataclass
+class Library:
+	images: dict[Image, list[Surface]]
+	sounds: dict[Sound, list[SoundFile]]
+
+I: Library
+
+def init() -> None:
+	global I
+	I = Library(images={}, sounds={})
+
+def get_sounds(sound: Sound) -> list[SoundFile]:
+	if sound in I.sounds: return
+	I.sounds[sound] = loaders.sounds(sound.value)
+	return I.sounds[sound]
+
+def play_sound(sound: Sound, forever: bool = False) -> bool:
+	loops = -1 if forever else 0
+	variants = get_sounds(sound)
+	if len(variants) == 0: return False
+	random.choice(variants).play(loops)
+	return True
+
+def stop_sound(sound: Sound) -> None:
+	for variant in get_sounds(sound):
+		variant.stop()
