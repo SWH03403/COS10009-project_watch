6eed6a30af6e279ebb436718acdb3b61b4d7b2f1
SWH03403 <106224482@student.swin.edu.au>
2026-04-11 07:22:23 +0700

    feat: routine for death by falling

--- game/assets/deaths.py
+++ game/assets/deaths.py
@@ -1,6 +1,10 @@
 from enum import Enum, auto
 import sys
+from time import sleep
 import pygame
+
+from . import library
+from .library import Sound
 
 class Cause(Enum):
 	SYSTEM = auto()
@@ -13,7 +17,19 @@ def kill_application(ignore_god: bool = False) -> None:
 	pygame.quit()
 	sys.exit()
 
+def flash(color: str, delay: float) -> None:
+	from game import engine
+	engine.get_screen().fill(color)
+	engine.update()
+	sleep(delay)
+
 def execute(cause: Cause = Cause.SYSTEM) -> None:
+
 	match cause:
 		case Cause.SYSTEM:
 			kill_application(True)
+		case Cause.FALL:
+			library.play_sound(Sound.DEATH_FALL)
+			flash("red3", .1)
+			flash("black", .4)
+			kill_application()
